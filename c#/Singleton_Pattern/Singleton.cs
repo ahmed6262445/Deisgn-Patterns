a9c63e317fe6e8d7b3c84423036b6553992c98f3
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-03-16 13:11:00 +0500

    Added c# Singleton Class

---
+++ c#/Singleton_Pattern/Singleton.cs
@@ -0,0 +1,18 @@
+namespace Singleton_Pattern
+{
+    public class Singleton
+    {
+        private Singleton() { }
+        private static Singleton _instance=null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if(_instance == null)
+                    _instance = new Singleton();
+                return _instance;
+            }
+        }
+    }
+}
