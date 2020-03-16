a8ecc885bf4fbad5d5a974a7eb1daefc67de47c6
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-03-16 13:28:05 +0500

    Changes in Singleton

--- c#/Singleton_Pattern/Singleton.cs
+++ c#/Singleton_Pattern/Singleton.cs
@@ -3,16 +3,18 @@ namespace Singleton_Pattern
     public class Singleton
     {
         private Singleton() { }
-        private static Singleton _instance=null;
+        private static Singleton _instance = null;
 
         public static Singleton Instance
         {
             get
             {
-                if(_instance == null)
+                if (_instance == null)
                     _instance = new Singleton();
                 return _instance;
             }
         }
+
+        public int Data { get; set; }
     }
 }
