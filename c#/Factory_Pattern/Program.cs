88eca9ff271ebdb8bd7f794c1d1a2973fe77b132
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-04-10 17:13:46 +0500

    Circle Class Added

--- c#/Factory_Pattern/Program.cs
+++ c#/Factory_Pattern/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Factory_Pattern
 {
+    interface Shape
+    {
+        public void Draw();
+    }
+
+    public class Circle:Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Inside Circle::Draw Method\n");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
